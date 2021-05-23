5ff1ca9509166de739e9ddcb3b0bdeac44fc7f85
DESKTOP-63EFDL5\Kamil <magicallbunny@gmail.com>
2021-05-23 19:52:55 +0200

    Add access to buttons elements and basic reaction on click

---
+++ assets/scripts/app.js
@@ -0,0 +1,22 @@
+const keyboard = document.querySelector('.keyboard');
+const displayCalc = document.querySelector('.calculations');
+
+const buttonsCollection = [];
+
+for (let i = 0; i < 17; i++) {
+    buttonsCollection.push(keyboard.children[i]);
+}
+
+for (const btn of buttonsCollection) {
+    btn.addEventListener("click", readButton);
+}
+
+// function outputCalculations(outputValue) {
+//     let displayText = displayCalc.textContent;
+//     displayText += outputValue;
+//     displayCalc.textContent = displayText;
+// }
+
+function readButton() {
+    console.log("click");
+}
