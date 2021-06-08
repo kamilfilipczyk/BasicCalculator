5a42d9abfde9aa1045ffbf6fc8441bd9d5f01f83
DESKTOP-63EFDL5\Kamil <magicallbunny@gmail.com>
2021-06-08 14:06:50 +0200

    Refactor the basic logic of the app (operations on intigers, showing result, clear funcionality)

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,31 +1,30 @@
 const keyboard = document.querySelector('.keyboard');
 const displayCalc = document.querySelector('.calculations');
+const defaultDisplayCalc = document.querySelector('#default-output');
 
 const buttonsCollection = [];
+let firstNumber = "";
+let secondNumber = "";
+let typeOfOperation;
+let result;
+let nextNumber = false; //holds an information if any arithmetic symbol was used (if so: true)
 
+
+const ADDITION = "addition";
+const SUBTRACTION = "subtraction";
+const MULTIPLICATION = "multiplication";
+const DIVISION = "division";
+
+//selects every button on the keyboard
 for (let i = 0; i < keyboard.children.length; i++) {
     buttonsCollection.push(keyboard.children[i]);
 }
 
+//adds listeners to every button
 for (const btn of buttonsCollection) {
     btn.addEventListener("click", readButton.bind(btn));
 }
 
-function manageInput(inputValue) {
-    if (inputValue === "C" ||
-        inputValue === "CE" ||
-        inputValue === "+/-" ||
-        inputValue === "=" ||
-        inputValue === "%") {
-        return;
-    } else if (inputValue === ".") {
-        outputCalculations(inputValue);
-        return;
-    } else if (displayCalc.innerHTML === "0") {
-        displayCalc.innerHTML = "";
-    }
-    outputCalculations(inputValue);
-}
 
 function outputCalculations(outputValue) {
     let displayText = displayCalc.innerHTML;
@@ -33,6 +32,115 @@ function outputCalculations(outputValue) {
     displayCalc.innerHTML = displayText;
 }
 
+function clearAll() {
+    let displayText = displayCalc.innerHTML;
+    displayCalc.innerHTML = "";
+    resetMemory();
+}
+
+function showResult() {
+    let displayText = displayCalc.innerHTML;
+    displayCalc.innerHTML = result;
+}
+
+function resetMemory() {
+    nextNumber = false;
+    firstNumber = "";
+    secondNumber = "";
+    result = 0;
+    typeOfOperation = "";
+}
+
+function assignToNumber(inputValue) {
+    if (nextNumber === false) {
+        firstNumber += inputValue;
+    } else {
+        secondNumber += inputValue;
+    }
+}
+
+function calculateResult() {
+    switch (typeOfOperation) {
+        case ADDITION:
+            result = parseFloat(firstNumber) + parseFloat(secondNumber);
+            break;
+        case SUBTRACTION:
+            result = parseFloat(firstNumber) - parseFloat(secondNumber);
+            break;
+        case MULTIPLICATION:
+            result = parseFloat(firstNumber) * parseFloat(secondNumber);
+            break;
+        case DIVISION:
+            result = parseFloat(firstNumber) / parseFloat(secondNumber);
+            break;
+
+        default:
+            break;
+    }
+}
+
 function readButton() {
     manageInput(this.innerHTML);
+}
+
+function manageInput(pressedButton) {
+    switch (pressedButton) {
+        case "C":
+            clearAll();
+            break;
+        case "CE":
+
+            break;
+        case "%":
+
+            break;
+        case "+/-":
+
+            break;
+        case ".":
+
+            break;
+        case "=":
+            if (nextNumber === true && secondNumber !== "") {
+                nextNumber = false;
+                calculateResult();
+                showResult();
+                resetMemory();
+            }
+            break;
+        case "+":
+            if (nextNumber === false) {
+                nextNumber = true;
+                typeOfOperation = ADDITION;
+                outputCalculations(pressedButton);
+            }
+            break;
+        case "-":
+            if (nextNumber === false) {
+                nextNumber = true;
+                typeOfOperation = SUBTRACTION;
+                outputCalculations(pressedButton);
+            }
+            break;
+        case "x":
+            if (nextNumber === false) {
+                nextNumber = true;
+                typeOfOperation = MULTIPLICATION;
+                outputCalculations(pressedButton);
+            }
+            break;
+        case "/":
+            if (nextNumber === false) {
+                nextNumber = true;
+                typeOfOperation = DIVISION;
+                outputCalculations(pressedButton);
+            }
+            break;
+
+        default:
+            //only works if a number was clicked
+            outputCalculations(pressedButton);
+            assignToNumber(pressedButton);
+            break;
+    }
 }
